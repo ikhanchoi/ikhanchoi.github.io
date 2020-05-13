ef387b142f3d724b37b87165fd6885e50655c4f6
ikhanchoi <dlrgks623@naver.com>
2020-05-13 09:03:23 +0900

    05/13/20

--- push.sh
+++ push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sourcedir=./../mathnotes/essays
 assetdir=./assets/postgenerator
-date=$(grep '\date{' $dir/$name/$name.tex | rev | cut -c 1-10 | rev)
+date=$(grep '\date{' $sourcedir/$name/$name.tex | rev | cut -c 1-10 | rev)
 
 # 포스트 생성
 for entry in $sourcedir/*; do
 	name=${entry##*/}
-	pandoc --lua-filter=$assetdir/filter.lua --template=$assetdir/template.md --shift-heading-level=1 --atx-header --standalone -N -o ../../_posts/$date-$name.md $dir/$name/$name.tex
+	pandoc --lua-filter=$assetdir/filter.lua --template=$assetdir/template.md --shift-heading-level=1 --atx-header --standalone -N -o ../../_posts/$date-$name.md $sourcedir/$name/$name.tex
 done
 
 # 푸시
